c52faed9c1fcbcc7c2c284fbebe45588b9f490af
numeroflip <aron.berenyi@tuta.io>
2023-04-24 21:40:35 +0200

    handle cases, when a single template is present

--- README.md
+++ README.md
@@ -1,17 +1,32 @@
 # Auto Template Prompt
-
 This is a simple Obsidian plugin. (https://obsidian.md)  
-It triggers the template prompt when creating a note. (And the Template plugin is enabled)
+
+It triggers the template prompt when creating a note. (If you have multiple templates)
+If you have a single template, it get's applied every time a note is created.
+
+## Prerequisites
+It depends on the core `Templates` plugin to be enabled, and a templates folder is being assigned.
+
+## Usage
+Make sure you have one or more template.  
 
 If the built-in template plugin serves your needs weel, this one might enhance your experience. 
 
+## Behavior
+### 1. Create a new note
+![image](https://user-images.githubusercontent.com/46031874/233847364-48e0ca1e-f8cc-4aff-a582-b9c9fdd215b8.png)
+### 2.a The template prompt is automatically triggered (If you have multiple templates)
+![image](https://user-images.githubusercontent.com/46031874/233847405-6ad376cc-2d76-42fe-ba12-3e173d38163c.png)
+
+### 2.b The template is automatically applied (If you have a single template)
+
+## Why it was made
 I found myself manually triggering a template, whenewer I created a note, which was a bit cumbersome, especially on mobile. 
 Now, a prompt is automatically triggered, which simplifies my workflow.
 
 It probably won't work well with other plugins eg Templater or daily notes. 
 
-## Behavior
-### 1. Create a new note
-![image](https://user-images.githubusercontent.com/46031874/233847364-48e0ca1e-f8cc-4aff-a582-b9c9fdd215b8.png)
-### 2. The template prompt is automatically triggered
-![image](https://user-images.githubusercontent.com/46031874/233847405-6ad376cc-2d76-42fe-ba12-3e173d38163c.png)
+## Bugs
+### The template is applied again, when you duplicate a file. 
+It happens when you have a single template, which is usually automatically applied, and you duplicate a file.
+You can use the Undo command if that happens. (Ctrl + Z) 
--- main.ts
+++ main.ts
@@ -1,19 +1,71 @@
-import { Plugin } from 'obsidian';
+import { Plugin, TAbstractFile, TFile } from 'obsidian';
 
+const DELAY_IN_MILLISECONDS = 100;
 
 export default class AutoTemplatePromptPlugin extends Plugin {
 	isReady = false;
+
 	async onload() {
 		this.app.workspace.onLayoutReady(() => {
 			this.isReady = true
 		})
+
 		this.registerEvent(
-			this.app.vault.on("create", () => {
-				if (!this.isReady) {
+			this.app.vault.on("create", async (newFile) => {
+				if (!this.isReady || !this.isMarkdown(newFile)) {
 					return;
 				}
-				// @ts-expect-error
-				this.app.commands.executeCommandById('insert-template');
+
+				const templatesFolder = await this.getTemplatesFolder()
+
+				if (!templatesFolder) {
+					return
+				}
+
+				if (newFile.path.startsWith(templatesFolder)) {
+					return; // Do not trigger when creating a template
+				}
+
+				const templateFiles = this.getFilesInFolder(templatesFolder)
+				const templateFileCount = templateFiles?.length
+
+				if (!templateFileCount) {
+					return
+				}
+
+				/**
+				 * For some reason, if there is a single template, 
+				 * immediately calling the 'insert-template' command does not work. 
+				 * BUT, if it's called a bit later, it does. Hence the minor timeout.
+				 */
+				if (templateFileCount === 1) {
+					setTimeout(this.insertTemplate, DELAY_IN_MILLISECONDS)
+				}
+
+				if (templateFileCount > 1) {
+					this.insertTemplate();
+				}
 			}))
+	}
+
+	async getTemplatesFolder() {
+		//@ts-expect-error
+		const templatesSettings = await this.app.vault.readConfigJson('templates')
+		return templatesSettings?.folder
+
+	}
+
+	getFilesInFolder(folderPath: string) {
+		const files = this.app.vault.getAllLoadedFiles().filter(file => file.path.startsWith(`${folderPath}/`))
+		return files
+	}
+
+	isMarkdown(file: TAbstractFile) {
+		return file.path.endsWith('.md')
+	}
+
+	insertTemplate() {
+		//@ts-expect-error
+		this.app.commands.executeCommandById('insert-template');
 	}
 }
