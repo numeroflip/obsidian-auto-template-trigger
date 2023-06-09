1fae66fed576d581362b49f5d079dc9309e57a99
numeroflip <aron.berenyi@tuta.io>
2023-06-09 17:26:25 +0200

    fix: note-composer bug
    Fixing an issue, where the 'Extract current selection'
    command caused incorrect behavior
    
    a check is added, to only trigger if the created file is in focus

--- main.ts
+++ main.ts
@@ -20,8 +20,9 @@ export default class AutoTemplatePromptPlugin extends Plugin {
 				}
 
 				const isJustCreated = createdFile.stat.ctime === createdFile.stat.mtime
+				const isFileInFocus = this.app.workspace.getActiveFile()?.path === createdFile.path;
 
-				if (!isJustCreated) {
+				if (!isJustCreated || !isFileInFocus) {
 					return
 				}
 
